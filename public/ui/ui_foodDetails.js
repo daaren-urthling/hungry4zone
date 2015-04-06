585fd0ae4c7f1b9e08e36a0e542e1b1ce29c86d0
daaren-urthling <daaren.urthling@hotmail.com>
2015-04-06 21:18:25 +0200

    applicato Bootstrap (fine)

--- public/ui/ui_foodDetails.js
+++ public/ui/ui_foodDetails.js
@@ -16,40 +16,50 @@ app.controller('FoodDetailController', ['$scope', '$routeParams', 'Foods', '$loc
 
   //-----------------------------------------------------------------------------
   $scope.onAddClicked = function(){
-    if(!$scope.food.name || $scope.food.name.length < 1) return;
+    if(!$scope.food.name || $scope.food.name.length < 1)
+    {
+      $scope.alert = { type : "danger", msg : 'Indicare il nome dell\'alimento'};
+      return;
+    }
+
+    var newFood = $scope.food.name;
 
     $scope.food.$save(function(result){
       if (!result.success && result.id)
-        $scope.alertMessage = 'Alimento già presente';
+        $scope.alert = { type : "danger", msg : 'Alimento già presente'};
       else
-        $location.url('/foods');
+        $location.url('/foods/'+ angular.toJson({ "type" : "success", "msg" : "Inserito un nuovo alimento: " + newFood}));
     });
   };
 
   //-----------------------------------------------------------------------------
   $scope.onUpdateClicked = function(){
+    var changedFood = $scope.food.name;
+
     Foods.update({id: $scope.food._id}, $scope.food, function(){
-      $location.url('/foods');
+      $location.url('/foods/'+ angular.toJson({ "type" : "success", "msg" : "Alimento modificato: " + changedFood}));
     });
   };
 
   //-----------------------------------------------------------------------------
   $scope.onRemoveClicked = function(){
+
+    var removedFood = $scope.food.name;
+
     Foods.remove({id: $scope.food._id}, function(){
-      $location.url('/foods');
+      $location.url('/foods/'+ angular.toJson({ "type" : "warning", "msg" : "Alimento eliminato: " + removedFood}));
     });
   };
 
   //-----------------------------------------------------------------------------
   $scope.onFoodNameChanged = function(){
-    $scope.alertMessage ='';
+    $scope.alert = null;
     if (!$scope.isNew) return;
 
     Foods.search({name: $scope.food.name },
       function(result) {
-        console.log('done: ', result);
         if (result.success)
-          $scope.alertMessage = 'Alimento già presente';
+          $scope.alert = { type : "danger", msg : 'Alimento già presente'};
       },
       function(httpResponse) {
         console.log('something wrong: ', httpResponse);
@@ -59,5 +69,9 @@ app.controller('FoodDetailController', ['$scope', '$routeParams', 'Foods', '$loc
 //-----------------------------------------------------------------------------
   $scope.onFoodSourceChanged= function(){
     $scope.food.source = parseInt($scope.food.source);
+  };
+  //-----------------------------------------------------------------------------
+  $scope.onCloseAlert = function(){
+    $scope.alert = null;
   };
 }]);
--- public/ui/ui_foods.js
+++ public/ui/ui_foods.js
@@ -2,13 +2,22 @@
 // FoodsController - controller for ui_foods.html
 //=============================================================================
 
-app.controller('FoodsController', ['$scope', 'Foods', '$location', function ($scope, Foods, $location) {
+app.controller('FoodsController', ['$scope', 'Foods', '$location', '$routeParams', function ($scope, Foods, $location, $routeParams) {
   $scope.foods = Foods.query();
 
   console.log('foods:',$scope.foods);
 
+  $scope.alert = null;
+  if ($routeParams.alert)
+    $scope.alert = angular.fromJson($routeParams.alert);
+
   //-----------------------------------------------------------------------------
   $scope.onAddClicked = function(){
     $location.url('/foodDetail/0');
+  };
+
+  //-----------------------------------------------------------------------------
+  $scope.onCloseAlert = function(){
+    $scope.alert = null;
   };
 }]);
