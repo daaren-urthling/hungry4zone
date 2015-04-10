5a16014ffc277d2f3139213561e83c76a7454b23
daaren-urthling <daaren.urthling@hotmail.com>
2015-04-10 23:38:04 +0200

    calcolatore pasti (quasi) completato

--- public/ui/ui_calculator.js
+++ public/ui/ui_calculator.js
@@ -2,23 +2,72 @@
 // CalculatorController - controller for ui_calculator.html
 //=============================================================================
 
-app.controller('CalculatorController', ['$scope', '$rootScope', 'Meals', 'Foods', function ($scope, $rootScope, Meals, Foods) {
+app.controller('CalculatorController', ['$scope', '$rootScope', 'Meals', 'Foods', 'FoodTypes', function ($scope, $rootScope, Meals, Foods, FoodTypes) {
   $scope.meal = new Meals();
-  $scope.meal.totCalories = 100;
 
   $scope.foods = Foods.query();
 
   //-----------------------------------------------------------------------------
+  function recalculate(mealItem){
+    if (mealItem) {
+      mealItem.totProteins      = 0.0;
+      mealItem.totFats          = 0.0;
+      mealItem.totCarbohydrates = 0.0;
+
+      if (mealItem.food && mealItem.foodType) {
+        mealItem.totProteins      = mealItem.qty * mealItem.food.proteins       * (mealItem.foodType.proteineAbsorption / 100.00) / 100.00;
+        mealItem.totFats          = mealItem.qty * mealItem.food.fats           / 100.00;
+        mealItem.totCarbohydrates = mealItem.qty * mealItem.food.carbohydrates  / 100.00;
+      }
+    }
+
+    $scope.meal.totProteins      = 0.0;
+    $scope.meal.totFats          = 0.0;
+    $scope.meal.totCarbohydrates = 0.0;
+    $scope.meal.mealItems.forEach(function(mealItem){
+      $scope.meal.totProteins      += (typeof mealItem.totProteins      === "undefined") ? 0 : mealItem.totProteins;
+      $scope.meal.totFats          += (typeof mealItem.totFats          === "undefined") ? 0 : mealItem.totFats;
+      $scope.meal.totCarbohydrates += (typeof mealItem.totCarbohydrates === "undefined") ? 0 : mealItem.totCarbohydrates;
+    });
+
+    $scope.meal.blockProteins      = $scope.meal.totProteins * 0.14;
+    $scope.meal.blockFats          = $scope.meal.totFats * 0.32;
+    $scope.meal.blockCarbohydrates = $scope.meal.totCarbohydrates * 0.11;
+
+    $scope.meal.totCalories = Math.ceil($scope.meal.totProteins * 4 + $scope.meal.totFats * 9 + $scope.meal.totCarbohydrates * 4);
+    $scope.meal.mealRatio = $scope.meal.totCarbohydrates !== 0.0 ?
+                              Math.floor(($scope.meal.totProteins / $scope.meal.totCarbohydrates) * 100.00) / 100.00 :
+                              100.00;
+  }
+
+  //-----------------------------------------------------------------------------
   $scope.onFoodSelected = function($item, $index, mealItem)  {
-    console.log($item, $index, mealItem);
+    FoodTypes.get({id: mealItem.food.type }, function(result) {
+      if (result.success)
+      {
+        mealItem.foodType = result.data;
+        recalculate(mealItem);
+      }
+    });
     if ($index == $scope.meal.mealItems.length - 1)
       $scope.meal.mealItems.push({});
   };
 
   //-----------------------------------------------------------------------------
+  $scope.onQtyChanged = function(mealItem){
+    if (!mealItem)
+      return;
+
+    recalculate(mealItem);
+
+    // need to call manually $apply as called via onchange (not ng-change)
+    $scope.$apply();
+  };
+
+  //-----------------------------------------------------------------------------
   $scope.onRemoveLineClicked = function($index){
-    console.log('remove ',$index);
     $scope.meal.mealItems.splice($index,1);
+    recalculate();
     if ($scope.meal.mealItems.length < $scope.meal.minLength)
       $scope.meal.mealItems.push({});
   };
@@ -26,5 +75,6 @@ app.controller('CalculatorController', ['$scope', '$rootScope', 'Meals', 'Foods'
   //-----------------------------------------------------------------------------
   $scope.onRemoveAllClicked = function(){
     $scope.meal = new Meals();
+    recalculate();
   };
 }]);
--- routes/rt_foodTypes.js
+++ routes/rt_foodTypes.js
@@ -1,24 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var Result = require('../models/result.js');
+
+//=============================================================================
+module.exports = router;
+
+var foodTypes = [
+  { type : "carne", proteineAbsorption : 100 },
+  { type : "cereali", proteineAbsorption : 	40 },
+  { type : "frutta", proteineAbsorption : 	30 },
+  { type : "funghi", proteineAbsorption : 	50 },
+  { type : "grassi", proteineAbsorption : 	100 },
+  { type : "latticini", proteineAbsorption : 	100 },
+  { type : "legumi", proteineAbsorption : 	68 },
+  { type : "noci", proteineAbsorption : 	52 },
+  { type : "ortaggi", proteineAbsorption : 	50 },
+  { type : "pesce", proteineAbsorption : 	100 },
+  { type : "soia", proteineAbsorption : 	100 },
+  { type : "uova", proteineAbsorption : 	100 },
+];
+
+//-----------------------------------------------------------------------------
+function find(type) {
+  for (i = 0; i < foodTypes.length; i++) {
+    if (foodTypes[i].type == type)
+      return foodTypes[i];
+  }
+  return null;
+}
 
 // query          (GET /)
 //-----------------------------------------------------------------------------
 router.get('/', function(req, res, next) {
-  var foodTypes = [
-    { type : "carne", proteineAbsorption : 100 },
-    { type : "cereali", proteineAbsorption : 	40 },
-    { type : "frutta", proteineAbsorption : 	30 },
-    { type : "funghi", proteineAbsorption : 	50 },
-    { type : "grassi", proteineAbsorption : 	100 },
-    { type : "latticini", proteineAbsorption : 	100 },
-    { type : "legumi", proteineAbsorption : 	68 },
-    { type : "noci", proteineAbsorption : 	52 },
-    { type : "ortaggi", proteineAbsorption : 	50 },
-    { type : "pesce", proteineAbsorption : 	100 },
-    { type : "soia", proteineAbsorption : 	100 },
-    { type : "uova", proteineAbsorption : 	100 },
-  ];
   res.json(foodTypes);
 });
 
-module.exports = router;
+// get            (GET /:id)
+//-----------------------------------------------------------------------------
+router.get('/:id', function(req, res, next) {
+  var foodType = find(req.params.id);
+
+  if (foodType)
+    res.send(new Result(true, req.params.id, foodType));
+  else
+    res.send(new Result(false, req.params.id));
+});
